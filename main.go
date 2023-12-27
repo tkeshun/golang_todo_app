39aa7d879cf492800959eaea03231c1e077e6ab3
tkeshun <shun9646@gmail.com>
2023-12-27 14:57:52 +0900

    portを変えられるようにリファクタ

--- main.go
+++ main.go
@@ -4,50 +4,63 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-// run関数にhttpサーバーの処理を分離
-func main(){
+// run関数にhttpサーバーの処理を分離,  curl http://localhost:port番号/メッセージ
+func main() {
+	// 引数でポート番号を指定できるように変更
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
 	// すべての goroutine が終わるのを待つ
 	// エラーハンドリング
-	if err := run(context.Background()); err != nil {
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
 // 異常時は停止せず，error型を返す．
 // context.Contextの値を引数にとり，外部からのキャンセル伝播で停止する
 // context.Background() で空のContextを生成
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	// http.Server型．ListenAndServeメソッドでサーバーを起動する．
 	s := &http.Server{
-		Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}), 
+		}),
 	}
-	// 
+
 	eg, ctx := errgroup.WithContext(ctx)
 	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
 		// httpリクエストを受け付ける
 		// ErrServerClosedはサーバーが正常終了したことを示すのでエラーハンドリングから除外
-		if err := s.ListenAndServe(); err != nil && 
-		err != http.ErrServerClosed {
+		// Serveの引数にListenerを指定
+		if err := s.Serve(l); err != nil &&
+			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
-			 // eg.Go() で実行された関数にエラーを返すものがいれば、ctx はキャンセルされる
+			// eg.Go() で実行された関数にエラーを返すものがいれば、ctx はキャンセルされる
 			return err
 		}
 		return nil
-	}) 
+	})
 	// Done通知があったらサーバーを終了させる
-	<- ctx.Done()
+	<-ctx.Done()
 	// context.Background() で空のContextを生成
 	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v",err)
+		log.Printf("failed to shutdown: %+v", err)
 	}
 	// Goメソッドで起動した別ゴルーチンの終了を待つ
 	return eg.Wait()
--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 
@@ -16,13 +17,21 @@ func TestRun(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	// errgroupで戻り値にエラーを含むゴルーチンを扱いやすくする
 	eg, ctx := errgroup.WithContext(ctx)
+	// Listenerの設定. port番号に0を指定しておくと利用可能なポートを動的に選んでくれる
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port %v", err)
+	}
 	// ゴルーチンでhttpサーバーの起動
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, l)
 	})
-	// httpリクエストの送信 
+	// httpリクエストの送信
 	in := "message"
-	rsp, err := http.Get("http://localhost:18080/"+in)
+	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	// どのポート番号で通信しているかログに残す
+	t.Logf("try request to %q", url)
+	rsp, err := http.Get(url)
 	// 送信のエラーハンドリング
 	if err != nil {
 		t.Errorf("failed to get: %+v", err)
@@ -31,15 +40,15 @@ func TestRun(t *testing.T) {
 	// 関数が終了する前に確実にクローズ処理が行われるようにする
 	defer rsp.Body.Close()
 	// Bodyから値を読み出す
-	got ,err := io.ReadAll(rsp.Body)
+	got, err := io.ReadAll(rsp.Body)
 	if err != nil {
 		t.Fatalf("failed to read body: %v", err)
 	}
 
 	// 期待値と実際の値の比較
-	want := fmt.Sprintf("Hello, %s!",in)
+	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
-		t.Errorf("want %q,but got %q",want,got)
+		t.Errorf("want %q,but got %q", want, got)
 	}
 	// キャンセルとゴルーチンの終了待ち
 	cancel()
